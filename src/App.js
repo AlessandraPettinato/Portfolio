a406823f77412713e7c7a3591ca5c9af8d833177
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-08-17 14:34:45 +0200

    style: set general style. set main grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./App.css";
 
 export default function App() {
 	return (
-		<div>
-			<h1>Hello from App!</h1>
-		</div>
+		<>
+			<h1 className="title">Alessandra Pettinato</h1>
+		</>
 	);
 }
