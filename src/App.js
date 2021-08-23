2c6279ceafd58228be41855bdd8453223e65de3e
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-08-23 19:56:24 +0200

    feat: add login time and hour

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
+	const date = new Date().toDateString();
+	const hours = new Date().getHours();
+	const minutes = new Date().getMinutes();
+	const seconds = new Date().getSeconds();
+
+	const login = `${date} ${hours}:${minutes}:${seconds}`;
+	console.log(date);
+
 	return (
 		<>
-			<h1 className="title">Alessandra Pettinato</h1>
+			<p className="login">Last login: {login}</p>
+			<h1 className="title"> ~ Alessandra Pettinato</h1>
+			<p className="intro">
+				Some cute words about me. Lorem ipsum dolor sit amet, consectetur
+				adipiscing elit. Donec magna felis, tempor tempus sodales eu, laoreet
+				quis ipsum. Ut sed pharetra ante. Sed ultrices arcu quis risus interdum
+				mollis id a ex. Praesent vel consectetur leo. Vestibulum purus massa,
+				porttitor nec metus ac, gravida mattis mauris.{" "}
+			</p>
 		</>
 	);
 }
