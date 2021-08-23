e57c86cda006237cf16b676e769e4e472abf9f86
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-08-23 20:31:18 +0200

    style: add blinking cursor. Style accordingly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from "react";
+import { BsArrowRightShort } from "react-icons/bs";
 import "./App.css";
 
 export default function App() {
-	const date = new Date().toDateString();
+	const date = new Date().toDateString().slice(0, 10);
 	const hours = new Date().getHours();
 	const minutes = new Date().getMinutes();
 	const seconds = new Date().getSeconds();
 
 	const login = `${date} ${hours}:${minutes}:${seconds}`;
-	console.log(date);
 
 	return (
 		<>
 			<p className="login">Last login: {login}</p>
-			<h1 className="title"> ~ Alessandra Pettinato</h1>
-			<p className="intro">
+			<div className="user-container">
+				<BsArrowRightShort className="arrow" />
+				<p className="user" text="/Users/visitor">
+					/Users/visitor
+				</p>
+				<div className="blinking" />
+			</div>
+			{/* <p className="intro">
 				Some cute words about me. Lorem ipsum dolor sit amet, consectetur
 				adipiscing elit. Donec magna felis, tempor tempus sodales eu, laoreet
 				quis ipsum. Ut sed pharetra ante. Sed ultrices arcu quis risus interdum
 				mollis id a ex. Praesent vel consectetur leo. Vestibulum purus massa,
 				porttitor nec metus ac, gravida mattis mauris.{" "}
-			</p>
+			</p> */}
 		</>
 	);
 }
