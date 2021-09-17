96e3e4d7cf6373cbf86f2befe2d3c74d65380752
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-09-17 14:00:43 +0200

    refactor: modify resume component content. Move arrow/tilde's div in resume component.  style: add double border to text

--- src/AllProjects.js
+++ src/AllProjects.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BsArrowRightShort } from "react-icons/bs";
 
 export default function AllProjects() {
 	const date = new Date().toDateString().slice(0, 10);
@@ -12,16 +11,10 @@ export default function AllProjects() {
 		<div className="all-projects-container">
 			<header>
 				<p className="login">
-					Last login: <time>{login}</time> on <span>Alessandra Pettinato</span>
-					's <span style={{ color: "#41c731" }}>Portofolio</span>
+					Last login: <time>{login}</time> on Alessandra Pettinato's{" "}
+					<span style={{ color: "#41c731" }}>Portofolio</span>
 				</p>
 			</header>
-			<div className="tilde-blink-container">
-				<BsArrowRightShort className="arrow" />
-				<p className="tilde"> ~ </p>
-				{/* <p className="welcome-message"> WELCOME TO MY PORTFOLIO</p> */}
-				{/* <span className="blinking" /> */}
-			</div>
 		</div>
 	);
 }
