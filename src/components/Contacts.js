044a6083457721e3e793e27fa86c9ed3f06c96f8
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-09-18 11:14:55 +0200

    refactor: move header to App component. feat: add contacts component to hold contact links

---
+++ src/components/Contacts.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Contacts.css";
+
+export default function Resume() {
+	return (
+		<section className="contacts-container">
+			<aside>
+				<h1 className="contacts">CONTACTS</h1>
+				<>
+					<h1 className="handles">LinkedIn</h1>
+					<h1 className="handles">GitHub</h1>
+				</>
+				<h1></h1>
+			</aside>
+		</section>
+	);
+}
