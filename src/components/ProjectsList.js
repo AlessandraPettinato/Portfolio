b5b5e601a0f8daf75e175c32325e9b84188e1907
Alessandra Pettinato <alessandrapettinato@hotmail.it>
2021-12-16 16:52:46 +0100

    feat: add AdminPanel component. Little tweak to header

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -7,11 +7,13 @@ import SpaceCoachella from "./projects/SpaceCoachella";
 import Stopwatch from "./projects/Stopwatch";
 import Todo from "./projects/Todo";
 import Pokedex from "./projects/Pokedex";
+import AdminPanel from "./projects/AdminPanel";
 
 export default function ProjectsList() {
 	return (
 		<div className="all-projects-container">
 			{/* <Resume /> */}
+			<AdminPanel />
 			<Dravo />
 			<BrightFlash />
 			<SpaceCoachella />
